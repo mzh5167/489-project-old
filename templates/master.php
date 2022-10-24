69289fdaddcb34fcee801d78b405b2be2adbd187
Your Name <you@example.com>
2022-10-24 10:07:04 +0300

    Refactored templates/master.php

--- templates/master.php
+++ templates/master.php
@@ -1,23 +1,68 @@
 <?php
+
 /**
  * In order to use this class, you have to implement `main()`
  * 
  * You can optionally implement different attributes and methods as follows:
  * - `$title` is for the page title
  * - `$meta` is for extra meta tags
- * - etc
+ * - method `style` is used to add extra styles on a per page basis
+ * - method `script` is used to add extra scripts on a per page basis
  */
 abstract class masterLayout
 {
   protected $title = "Default title";
   protected $meta = "";
   protected $alert_message = "";
-  protected $style;
-
   abstract protected function main();
 
   protected function script()
   {
+  }
+  protected function style()
+  {
+  }
+  private static function navbar()
+  {
+  ?>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
+              Dropdown
+            </a>
+            <div class="dropdown-menu">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled">Disabled</a>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+
+
+<?php
   }
 
   public function doc()
@@ -40,50 +85,13 @@ abstract class masterLayout
       <!-- <link rel="stylesheet" href="../custom.css"> -->
 
       <title><?= $this->title ?></title>
-      <?php if (isset($style)) { ?>
-        <style>
-          <?= $style ?>
-        </style>
-      <?php } ?>
+      <?php
+      $this->style();
+      ?>
     </head>
 
     <body>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
-                Dropdown
-              </a>
-              <div class="dropdown-menu">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">Something else here</a>
-              </div>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled">Disabled</a>
-            </li>
-          </ul>
-          <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          </form>
-        </div>
-      </nav>
-
+      <?php self::navbar() ?>
       <?php if ("" !== $this->alert_message) { ?>
         <header>
           <div class="alert alert-primary border text-center" role="alert">
@@ -108,7 +116,8 @@ abstract class masterLayout
     </body>
 
     </html>
-<?php
+  <?php
   }
+
 }
 ?>
