39818286af6850206709e3fe14eff1472d33ac9a
mzh5167 <202007655@stu.uob.edu.bh>
2022-11-03 13:25:09 +0300

    Implemented add_movie.php
    - Fixed form in templates/add_movie.php
    - add_movie.php now adds movies properly

--- add_movie.php
+++ add_movie.php
@@ -1,6 +1,41 @@
 <?php
+
+require("util/connection.php");
 require("templates/add_movie.php");
 
-$page = new addMovieLayout();
-$page->doc();
-?>
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  // TODO: input validation
+  try {
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+
+    if (preg_match('/^(\d{1,2}):(\d{2})$/', $_POST["duration"], $matches)) {
+      $duration = ($matches[1] * 60) + $matches[2];
+    } else {
+      $page = new addMovieLayout();
+      $page->alert_message = "Invalid duration format";
+      $page->doc();
+      die();
+    }
+
+    $query = $db->prepare("INSERT INTO `movies`(title, releaseYear, lang, duration, rating, genre, `desc`)
+      VALUES (?, ?, ?, ?, ?, ?, ?)");
+    $status = $query->execute([
+      $_POST["title"],
+      $_POST["rYear"],
+      $_POST["lang"],
+      $duration,
+      $_POST["rating"],
+      $_POST["genre"],
+      $_POST["desc"]
+    ]);
+
+    $page = new addMovieLayout();
+    $page->alert_message = $status ? "Added movie successfully" : "Failed to add movie";
+    $page->doc();
+  } catch (PDOException $e) {
+    die($e->getMessage());
+  }
+} else {
+  $page = new addMovieLayout();
+  $page->doc();
+}
--- templates/add_movie.php
+++ templates/add_movie.php
@@ -13,43 +13,57 @@ class addMovieLayout extends masterLayout
   {
 ?>
     <div class="container" style="max-width: var(--breakpoint-md);">
-      <form>
+      <form method="post">
         <div class="card">
           <h4 class="card-header">Add movie</h4>
           <div class="card-body">
-            <div class="form-group">
-              <label for="movie-title">Title</label>
-              <input class="form-control" type="text" name="bday" id="movie-title">
+            <div class="form-row">
+              <div class="form-group col-md-6">
+                <label for="title-input">Title</label>
+                <input class="form-control" type="text" name="title" id="title-input">
+              </div>
+              <div class="form-group col-md-6">
+                <label for="rYear-input">Release year</label>
+                <input class="form-control" type="number" name="rYear" id="rYear-input">
+              </div>
             </div>
-            <div class="form-group">
-              <label for="movie-desc">Release year</label>
-              <input class="form-control" type="number" name="bday" id="movie-title">
-            </div>
-            <div class="form-group">
-              <label for="movie-desc">Duration</label>
-              <input class="form-control" type="time" max="02:00" min="00:00" name="bday" id="movie-title">
+            <div class="form-row">
+              <div class="form-group col-md-6">
+                <label for="duration-input">Duration</label>
+                <input class="form-control" type="time" max="02:00" min="00:00" name="duration" id="duration-input">
+              </div>
+              <div class="form-group col-md-6">
+                <label for="lang-input">Language</label>
+                <select class="form-control" name="lang" id="lang-input">
+                  <option value="en">English</option>
+                  <option value="ar">Arabic</option>
+                  <option value="Hu">Hindi</option>
+                </select>
+              </div>
             </div>
 
-            <div class="form-group">
-              <label for="rating-input">Rating</label>
-              <input class="form-control" type="number" max="5.0" min="0.0" step="0.1" name="rating" id="rating-input">
-            </div>
+            <div class="form-row">
+              <div class="form-group col-md-6">
+                <label for="rating-input">Rating</label>
+                <input class="form-control" type="number" max="5.0" min="0.0" step="0.1" name="rating" id="rating-input">
+              </div>
 
-            <!-- <label for="">Rating</label>
+              <!-- <label for="">Rating</label>
           <div class="form-group row px-3">
             <input class="form-control-range col-10" type="range" name="" id="rating-input" max="5" min="0" step="0.1">
             <div id="rating-val" class="col text-center">2.5</div>
           </div> -->
-            <div class="form-group">
-              <label for="genre-input">Genre</label>
-              <!-- <input class="col form-control" type="text" name="genre" > -->
-              <select class="form-control" name="genre" id="genre-select">
-                <?php
-                foreach (self::$genres as $key => $val) {
-                  echo "<option value='$key'>$val</option>";
-                }
-                ?>
-              </select>
+              <div class="form-group col-md-6">
+                <label for="genre-input">Genre</label>
+                <!-- <input class="col form-control" type="text" name="genre" > -->
+                <select class="form-control" name="genre" id="genre-input">
+                  <?php
+                  foreach (self::$genres as $key => $val) {
+                    echo "<option value='$key'>$val</option>";
+                  }
+                  ?>
+                </select>
+              </div>
             </div>
             <div class="form-group">
               <label for="movie-desc">Description</label>
