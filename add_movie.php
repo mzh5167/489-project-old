f5f56cd2765ad245de91decd5b705ec36b12e6fa
mzh5167 <202007655@stu.uob.edu.bh>
2022-11-07 15:26:01 +0300

    Got image upload to a somewhat working state in add_movie.php

--- add_movie.php
+++ add_movie.php
@@ -4,10 +4,40 @@ require("util/time.php");
 require("util/connection.php");
 require("templates/add_movie.php");
 
+$posters_dir = "assets/posters";
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   // TODO: input validation
   try {
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+
+    if (isset($_FILES['poster-img'])) {
+      $file = $_FILES['poster-img'];
+      if ($file['error'] != UPLOAD_ERR_OK) {
+        die("file was not uploaded properly");
+      }
+
+      $name = basename($file['name']);
+      // print_r($_SERVER);
+      // print_r(pathinfo($_SERVER['SCRIPT_FILENAME'])['dirname']);
+      // print_r(pathinfo($_SERVER['SCRIPT_FILENAME'])['dirname'] . "/$posters_dir");
+      // $target = pathinfo($_SERVER['SCRIPT_FILENAME'])['dirname'] . "/$posters_dir/$name";
+      echo "<br>";
+      print_r(
+        // pathinfo($file["tmp_name"])
+        pathinfo('./assets/posters')
+      );
+      $target = "$posters_dir/" . basename($file['name']);
+      echo "<br>";
+      echo "$file[tmp_name] -> $target <br>";
+      if (move_uploaded_file($file['tmp_name'], "$posters_dir/$name")) {
+      // if (move_uploaded_file($file['tmp_name'], "$target")) {
+        echo "uploaded successfully";
+      } else {
+        echo "wasn't uploaded successfully";
+      }
+    }
+    die();
 
     $duration = durationToSeconds($_POST["duration"]);
     if ($duration == null) {
