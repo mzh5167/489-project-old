395919dc0a4a63c27b0a59bb2d59bbe3c16a36e3
mzh5167 <202007655@stu.uob.edu.bh>
2022-11-08 19:46:55 +0300

    First fully function version of add_movie.php
    - Use `beginTransaction`
    - Added code to retrieve id and use it for poster filename

--- add_movie.php
+++ add_movie.php
@@ -11,6 +11,35 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   try {
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 
+    $duration = durationToSeconds($_POST["duration"]);
+    if ($duration == null) {
+      $page = new addMovieLayout();
+      $page->alert_message = "Invalid duration format";
+      $page->doc();
+      die();
+    }
+
+    $db->beginTransaction();
+
+    $query = $db->prepare("INSERT INTO `movies`(title, releaseYear, lang, duration, rating, genre, `desc`)
+      VALUES (?, ?, ?, ?, ?, ?, ?)");
+    $status = $query->execute([
+      $_POST["title"],
+      $_POST["rYear"],
+      $_POST["lang"],
+      $duration,
+      $_POST["rating"],
+      $_POST["genre"],
+      $_POST["desc"]
+    ]);
+
+    $movie_id = $db
+      ->query("SELECT LAST_INSERT_ID() AS id")
+      ->fetch(PDO::FETCH_ASSOC)['id'];
+    if (!isset($movie_id))
+      throw new PDOException("Could not fetch id");
+
+    // TODO: find a better way to provide feedback
     // Image upload
     if (isset($_FILES['poster-img'])) {
       $file = $_FILES['poster-img'];
@@ -28,39 +57,22 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         die("Please upload an image not '$type'");
       }
 
-      $target = "$posters_dir/" . basename($file['name']);
+      $ext = pathinfo($file['name'], PATHINFO_EXTENSION);
+      $target = "$posters_dir/$movie_id.$ext";
       if (move_uploaded_file($file['tmp_name'], $target)) {
-        echo "uploaded successfully";
+        // echo "uploaded successfully";
       } else {
-        echo "wasn't uploaded successfully";
+        die("wasn't uploaded successfully");
       }
     }
-    die();
 
-    $duration = durationToSeconds($_POST["duration"]);
-    if ($duration == null) {
-      $page = new addMovieLayout();
-      $page->alert_message = "Invalid duration format";
-      $page->doc();
-      die();
-    }
-
-    $query = $db->prepare("INSERT INTO `movies`(title, releaseYear, lang, duration, rating, genre, `desc`)
-      VALUES (?, ?, ?, ?, ?, ?, ?)");
-    $status = $query->execute([
-      $_POST["title"],
-      $_POST["rYear"],
-      $_POST["lang"],
-      $duration,
-      $_POST["rating"],
-      $_POST["genre"],
-      $_POST["desc"]
-    ]);
+    $db->commit();
 
     $page = new addMovieLayout();
     $page->alert_message = $status ? "Added movie successfully" : "Failed to add movie";
     $page->doc();
   } catch (PDOException $e) {
+    $db->rollBack();
     die($e->getMessage());
   }
 } else {
