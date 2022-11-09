687f8d366edf8bcf354ae78ba06610fa5c9d5864
mzh5167 <202007655@stu.uob.edu.bh>
2022-11-09 18:47:48 +0300

    Added more tables to database

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Nov 08, 2022 at 08:14 PM
+-- Generation Time: Nov 09, 2022 at 04:45 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -20,6 +20,79 @@ SET time_zone = "+00:00";
 --
 -- Database: `tmp0`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bookings`
+--
+
+CREATE TABLE `bookings` (
+  `id` int(11) NOT NULL,
+  `paymentMethod` enum('paypal','apple-pay','benefit-pay') NOT NULL,
+  `timeSlotId` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `seatStart` char(2) NOT NULL,
+  `seatEnd` char(2) NOT NULL,
+  `status` enum('pending','complete','canceled','suspended') NOT NULL DEFAULT 'pending',
+  `fee` decimal(3,1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `branches`
+--
+
+CREATE TABLE `branches` (
+  `id` int(11) NOT NULL,
+  `name` varchar(31) NOT NULL,
+  `addr` varchar(63) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `branches`
+--
+
+INSERT INTO `branches` (`id`, `name`, `addr`) VALUES
+(1, 'City centre cinema', 'city centre mall'),
+(2, 'Juffair Cinema', 'Juffair Oasis mall');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `halls`
+--
+
+CREATE TABLE `halls` (
+  `id` int(11) NOT NULL,
+  `letter` char(1) NOT NULL,
+  `branchId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `halls`
+--
+
+INSERT INTO `halls` (`id`, `letter`, `branchId`) VALUES
+(1, 'A', 1),
+(2, 'B', 1),
+(3, 'A', 2),
+(4, 'B', 2),
+(5, 'C', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `id` int(11) NOT NULL,
+  `name` varchar(31) DEFAULT NULL,
+  `email` varchar(31) DEFAULT NULL,
+  `message` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -47,6 +120,19 @@ INSERT INTO `movies` (`id`, `title`, `releaseYear`, `lang`, `duration`, `rating`
 (2, 'btmn', 2014, 'en', 90, '1.3', 'drama', 'descccc'),
 (29, 'hkjl', 2017, 'en', 99, '2.0', 'drama', 'jkjj'),
 (30, 'uywerui', 2034, 'en', 99, '2.0', 'drama', 'deskjl');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `timeSlots`
+--
+
+CREATE TABLE `timeSlots` (
+  `id` int(11) NOT NULL,
+  `time` datetime NOT NULL,
+  `branchId` int(11) NOT NULL,
+  `movieId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -79,10 +165,45 @@ INSERT INTO `users` (`id`, `fName`, `lName`, `email`, `birthday`, `hash`, `credi
 --
 
 --
+-- Indexes for table `bookings`
+--
+ALTER TABLE `bookings`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_bookings_timeslotid` (`timeSlotId`),
+  ADD KEY `fk_bookings_userid` (`userId`);
+
+--
+-- Indexes for table `branches`
+--
+ALTER TABLE `branches`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `halls`
+--
+ALTER TABLE `halls`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_halls_branchid` (`branchId`);
+
+--
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `movies`
 --
 ALTER TABLE `movies`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `timeSlots`
+--
+ALTER TABLE `timeSlots`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_timeslots_branchid` (`branchId`),
+  ADD KEY `fk_timeslots_movieid` (`movieId`);
 
 --
 -- Indexes for table `users`
@@ -95,16 +216,70 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `bookings`
+--
+ALTER TABLE `bookings`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `branches`
+--
+ALTER TABLE `branches`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `halls`
+--
+ALTER TABLE `halls`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `movies`
 --
 ALTER TABLE `movies`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
+-- AUTO_INCREMENT for table `timeSlots`
+--
+ALTER TABLE `timeSlots`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `bookings`
+--
+ALTER TABLE `bookings`
+  ADD CONSTRAINT `fk_bookings_timeslotid` FOREIGN KEY (`timeSlotId`) REFERENCES `timeSlots` (`id`),
+  ADD CONSTRAINT `fk_bookings_userid` FOREIGN KEY (`userId`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `halls`
+--
+ALTER TABLE `halls`
+  ADD CONSTRAINT `fk_halls_branchid` FOREIGN KEY (`branchId`) REFERENCES `branches` (`id`);
+
+--
+-- Constraints for table `timeSlots`
+--
+ALTER TABLE `timeSlots`
+  ADD CONSTRAINT `fk_timeslots_branchid` FOREIGN KEY (`branchId`) REFERENCES `branches` (`id`),
+  ADD CONSTRAINT `fk_timeslots_movieid` FOREIGN KEY (`movieId`) REFERENCES `movies` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
