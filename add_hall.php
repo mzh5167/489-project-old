73a1bc1c7af6c7b6a4fb909f8ebb311dc01a9adc
mzh5167 <202007655@stu.uob.edu.bh>
2022-11-09 18:35:28 +0300

    Implemented add_hall
    - Implemented add_hall form
    - Implemented backend code for add_hall
    - Added a query to check if a letter is already taken

---
+++ add_hall.php
@@ -0,0 +1,40 @@
+<?php
+
+require("util/connection.php");
+require("util/queries.php");
+require("templates/add_hall.php");
+
+try {
+  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $bid = $_POST["bid"];
+    $letter = $_POST["letter"];
+
+    // Ensure letter doesn't alrady exist
+    if (check_letter_exists($db, $bid, $letter)) {
+      die("letter already exists");
+    }
+
+    // Insert hall
+    $query = $db->prepare(
+      "INSERT INTO `halls`(letter, branchId)
+       VALUES (?, ?)"
+    );
+    $status = $query->execute([
+      $letter,
+      $bid
+    ]);
+
+    $alert_message = $status ? "Added branch successfully" : "Failed to add branch";
+  }
+
+  $branches = $db->query("SELECT id, `name` FROM branches");
+
+  // TODO: add an option to preselect hall letter if a branchId is passed with GET
+  //       and preselect the correspondent branch
+  $page = new addHallLayout($branches);
+  if (isset($alert_message))
+    $page->alert_message = $alert_message;
+  $page->doc();
+} catch (PDOException $e) {
+  die($e->getMessage());
+}
--- util/queries.php
+++ util/queries.php
@@ -1,7 +1,29 @@
 <?php
 /* Checks if the given email already exists in the database */
-function does_email_exist(PDO $db, string $email) : bool {
+function does_email_exist(PDO $db, string $email): bool
+{
   $query = $db->prepare("SELECT id FROM `users` WHERE email=?");
   $query->execute([$email]);
+  return $query->rowCount() != 0;
+}
+
+/**
+ * Checks if the given letter for a hall already exists within the branch
+ * 
+ * @param int $branchId   specifies which branch to search within
+ * @param string $letter  specifies which letter to search for
+ */
+function check_letter_exists(PDO $db, int $branchId, string $letter): bool
+{
+  $query = $db->prepare(
+    "SELECT id
+     FROM `halls`
+     WHERE letter=?
+     AND branchId=?"
+  );
+  $query->execute([
+    $letter,
+    $branchId
+  ]);
   return $query->rowCount() != 0;
 }
