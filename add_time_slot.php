c55c2c8f84ffa66bf4fe96918004df5d3d8e0152
mzh5167 <202007655@stu.uob.edu.bh>
2022-11-21 07:55:33 +0300

    Finished implementing add_time_slot
    - Added code to insert timeSlot
    - Added time and date inputs in page
    - Minor changes and improvements in page
    -

--- add_time_slot.php
+++ add_time_slot.php
@@ -5,10 +5,27 @@ require("templates/add_time_slot.php");
 
 try {
   if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $alert_message = "Not implemented";
-    return;
+    // TODO: validate data
+    $hid      = $_POST["hid"];
+    $mid      = $_POST["mid"];
+    $datetime = "$_POST[date] $_POST[time]";
+
+    $query = $db->prepare(
+      " INSERT INTO `timeSlots`(`time`, hallId, movieId)
+        VALUES (
+          STR_TO_DATE(?, '%Y-%m-%d %H:%i'),
+          ?,
+          ?
+        )"
+    );
+    $query->execute([
+      $datetime,
+      $hid,
+      $mid,
+    ]);
   }
 
+  // Fetch to use in the form
   $branches = $db->query("SELECT id, `name` FROM branches");
   $movies   = $db->query("SELECT id, title FROM movies");
 } catch (PDOException $e) {
--- templates/add_time_slot.php
+++ templates/add_time_slot.php
@@ -31,14 +31,14 @@ class addTimeSlotLayout extends masterLayout
                   <?php } ?>
                 </select>
               <?php } else { ?>
-                <input disabled type="text" class="form-control" id="movie-input">
+                <select class="form-control" name="mid" id="movie-input"> </select>
               <?php } ?>
             </div>
             <div class="form-row">
               <div class="form-group col-md-6">
                 <label for="branch-input">Branch</label>
                 <?php if (isset($this->branches)) { ?>
-                  <select class="form-control" name="bid" id="branch-input">
+                  <select class="form-control" id="branch-input">
                     <option hidden disabled selected value> -- Choose a branch -- </option>
                     <?php foreach ($this->branches as $branch) { ?>
                       <option value="<?= $branch['id'] ?>">
@@ -47,7 +47,7 @@ class addTimeSlotLayout extends masterLayout
                     <?php } ?>
                   </select>
                 <?php } else { ?>
-                  <input disabled type="text" class="form-control" id="branch-input">
+                  <select class="form-control" id="branch-input"> </select>
                 <?php } ?>
               </div>
               <div class="form-group col-md-6">
@@ -56,8 +56,18 @@ class addTimeSlotLayout extends masterLayout
                 </select>
               </div>
             </div>
+            <div class="form-row">
+              <div class="form-group col-md-6">
+                <label for="date-input">Time slot date</label>
+                <input class="form-control" type="date" name="date" id="date-input">
+              </div>
+              <div class="form-group col-md-6">
+                <label for="time-input">Time slot time</label>
+                <input class="form-control" type="time" name="time" id="time-input">
+              </div>
+            </div>
             <div class="form-group">
-              <button type="submit" class="btn btn-primary">Add movie</button>
+              <button type="submit" class="btn btn-primary">Add time slot</button>
             </div>
           </div>
         </div>
@@ -71,26 +81,32 @@ class addTimeSlotLayout extends masterLayout
   {
   ?>
     <script>
+      // TODO: move javascript into a separate file
       $(document).ready(() => {
         $("#branch-input").change((e) => {
           bid = e.target.value
           $.getJSON(`get_halls.php?bid=${bid}`)
             .done((data) => {
+              // Ensure halls exist
               if (data.length === 0) {
                 $("#hall-input")
+                  // Disable halls input
                   .attr("disabled", "disabled")
                   .empty()
                   .append("<option hidden disabled selected value> -- No halls exist -- </option>")
                 return
               }
+              // Convert data to options
               content = data.map((data) => {
                 return `<option value="${data.id}"> ${data.letter} </option>`;
-              });
+              })
               content.unshift("<option hidden disabled selected value> -- Choose a hall -- </option>")
               $("#hall-input")
+                // Enable halls input
                 .removeAttr("disabled")
+                // Add options
                 .empty()
-                .append(content);
+                .append(content)
             })
         })
       })
