e5432b8e1746f129b494fe354f1bf1966264213d
mzh5167 <202007655@stu.uob.edu.bh>
2022-11-21 12:29:06 +0300

    Implemented browse_time_slots
    - Time slots details are served via `get_time_slots.php` and recieved
      via AJAX in `templates/browse_time_slots.php`

---
+++ get_time_slots.php
@@ -0,0 +1,32 @@
+<?php
+
+require "util/connection.php";
+
+if (!isset($_GET["mid"]))
+  die();
+
+try {
+  $query = $db->prepare(
+    ' SELECT  -- Hall info
+              halls.letter AS hallLetter,
+              -- Branch info
+              branches.name as branchName,
+              -- timeSlot info
+              timeSlots.id,
+              DATE_FORMAT(`time`, "%Y-%m-%d") AS `date`,
+              DATE_FORMAT(`time`, "%H:%i") as `time`
+      FROM    `timeSlots`, `branches`, `halls`
+      WHERE   branches.id = branchId    -- Join on branch
+      AND     halls.id = hallId         -- Join on hall
+      AND     movieId = ?'
+  );
+  $query->execute([
+    $_GET['mid']
+  ]);
+
+  echo json_encode(
+    $query->fetchAll(PDO::FETCH_ASSOC)
+  );
+} catch (PDOException $e) {
+  die($e->getMessage());
+}
--- templates/browse_time_slots.php
+++ templates/browse_time_slots.php
@@ -3,46 +3,83 @@ require("master.php");
 
 class browseTimeSlotsLayout extends masterLayout
 {
-  private $header;
-  private $branches;
+  private $movies;
 
-  function __construct($header, $branches)
+  function __construct($movies)
   {
-    $this->branches = $branches;
-    $this->header = $header;
+    $this->movies = $movies;
   }
 
   function main()
   {
 ?>
-    <div class="table-responsive">
-      <table class="table table-striped">
-        <thead>
-          <tr>
-            <?php foreach (array_values($this->header) as $cell) { ?>
-              <th scope="col"><?= $cell ?></th>
-            <?php } ?>
-            <th></th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          <?php foreach ($this->branches as $branch) { ?>
-            <tr>
-              <?php foreach (array_keys($this->header) as $k) { ?>
-                <td><?= $branch[$k] ?></td>
-              <?php } ?>
-              <td>
-                <a href="edit_branch.php?id=<?= $branch['id'] ?>"> Edit </a>
-              </td>
-              <td>
-                <a href="browse_halls.php?id=<?= $branch['id'] ?>"> Browse Halls </a>
-              </td>
-            </tr>
+    <div class="form-group row">
+      <label for="movie-input" class="col-form-label col-md-3"> Movie </label>
+      <div class="col-md-9">
+        <select class="form-control" id="movie-input">
+          <option hidden disabled selected value> -- Choose a movie -- </option>
+          <?php foreach ($this->movies as $movie) { ?>
+            <option value="<?= $movie['id'] ?>">
+              <?= $movie['title'] ?>
+            </option>
           <?php } ?>
-        </tbody>
-      </table>
+        </select>
+      </div>
     </div>
+    <div id="ts-table" class="table-responsive" style="display: none;"></div>
+  <?php
+  }
+
+  function script()
+  {
+  ?>
+    <script>
+      newTag = (t, s) => `<${t}> ${s} </${t}>`
+      newRow = s => newTag("tr", s)
+      newCell = s => newTag("td", s)
+      // newRow = s => `<tr> ${s} </tr>`
+      // newCell = s => `<td> ${s} </td>`
+      header = ["ID", "Branch Name", "Hall letter", "Date", "Time"];
+
+      $(document).ready(() => {
+        $("#movie-input").change((e) => {
+          mid = e.target.value
+          $.getJSON(`get_time_slots.php?mid=${mid}`)
+            .done((data) => {
+              if (data.length === 0) {
+                // TODO: add message to be displayed in case no time slot exists
+                $("#ts-table")
+                  .css("display", "none")
+                return
+              }
+              // console.log(data)
+              content = '<table class="table table-striped">'
+              content += newTag(
+                "thead",
+                newRow(
+                  header.map(s => newCell(s)).join(" ")
+                )
+              );
+              content += "<tbody>"
+              data.forEach(element => {
+                j = element
+                content += `<tr>
+                  <td> ${element.id} </td>
+                  <td> ${element.branchName} </td>
+                  <td> ${element.hallLetter} </td>
+                  <td> ${element.date} </td>
+                  <td> ${element.time} </td>
+                </tr>`
+              });
+              content += '</tbody> </table>'
+              $("#ts-table")
+                .empty()
+                .append(content)
+                .css("display", "block")
+            })
+        })
+      })
+    </script>
 <?php
   }
 }
